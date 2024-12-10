d5dd70480ba7f085ea468c92ed6fc1754531b712
五十嵐翔 <igara1119@gmail.com>
2024-12-10 23:31:04 +0900

    update

--- Assets/Scenes/Stage1Scene/Stage1Scene_SceneParameter.cs
+++ Assets/Scenes/Stage1Scene/Stage1Scene_SceneParameter.cs
@@ -1,3 +1,5 @@
-public static class Stage1Scene_SceneParameter {
-    public static bool isTryAgain = false;
+public static class Stage1Scene_SceneParameter
+{
+  public static bool isTryAgain = false;
+  public static int hitPoint = 100;
 }

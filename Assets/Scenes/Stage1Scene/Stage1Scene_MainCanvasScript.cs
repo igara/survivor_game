eb412be66b1be67a74f4187518b87dd70604bc65
五十嵐翔 <igara1119@gmail.com>
2024-12-13 05:40:52 +0900

    UI調整

--- Assets/Scenes/Stage1Scene/Stage1Scene_MainCanvasScript.cs
+++ Assets/Scenes/Stage1Scene/Stage1Scene_MainCanvasScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class Stage1Scene_MainCanvasScript : MonoBehaviour
 {
@@ -21,13 +23,55 @@ public class Stage1Scene_MainCanvasScript : MonoBehaviour
   [SerializeField]
   private Transform uiTransform;
 
-  private float speed = 0.5f; // 移動速度
+  private float speed = 2f; // 移動速度
   private float inputThreshold = 0.1f;
   private float rotationSpeed = 5f;    // 角度変更のスムーズさ
   private float currentAngle = 0f;
 
+  private float timeRemaining = 180f;
+  [SerializeField]
+  private TMP_Text timerText;
+  private bool isRunning = true;
+
+  private int hp = 100;
+  [SerializeField]
+  private TMP_Text hpValueText;
+  [SerializeField]
+  private Slider hpSlider;
+
+  private int exp = 0;
+  private TMP_Text lavelValueText;
+  [SerializeField]
+  private Slider expSlider;
+
+  private int bullCount = 0;
+
+  void Start()
+  {
+    hpValueText.text = hp.ToString();
+    hpSlider.value = hp / 100;
+
+    expSlider.value = exp;
+  }
+
   void Update()
   {
+    if (isRunning)
+    {
+      if (timeRemaining > 0)
+      {
+        timeRemaining -= Time.deltaTime;
+        UpdateTimerDisplay();
+      }
+      else
+      {
+        timeRemaining = 0;
+        isRunning = false;
+        UpdateTimerDisplay();
+        OnTimerEnd(); // タイマーが終了したときの処理
+      }
+    }
+
     // 仮想スティックの入力値を取得
     float horizontal = joystick.Horizontal();
     float vertical = joystick.Vertical();
@@ -60,5 +104,18 @@ public class Stage1Scene_MainCanvasScript : MonoBehaviour
       gameOverCanvas.transform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, gameOverCanvas.transform.position.z);
       gameOverCanvas.transform.rotation = cameraTransform.rotation;
     }
+  }
+
+  void UpdateTimerDisplay()
+  {
+    int minutes = Mathf.FloorToInt(timeRemaining / 60);
+    int seconds = Mathf.FloorToInt(timeRemaining % 60);
+    timerText.text = $"{minutes:D2}:{seconds:D2}";
+  }
+
+  void OnTimerEnd()
+  {
+    // タイマー終了時の処理
+    Debug.Log("タイマーが終了しました！");
   }
 }
